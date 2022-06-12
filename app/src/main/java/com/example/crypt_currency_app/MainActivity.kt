e20b1161eb114c73cd847f53e468a56fb9ec3fe3
shimon <69175188+lil-shimon@users.noreply.github.com>
2022-06-12 20:50:36 +0900

    feat: 🎸 initial commit

---
+++ app/src/main/java/com/example/crypt_currency_app/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.crypt_currency_app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import com.example.crypt_currency_app.ui.theme.CryptocurrencyAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CryptocurrencyAppTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                }
+            }
+        }
+    }
+}
