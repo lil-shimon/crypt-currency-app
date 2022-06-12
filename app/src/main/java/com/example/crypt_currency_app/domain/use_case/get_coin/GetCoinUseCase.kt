98be35b9142292ac4f9fe98bb446b3cc28b6ce6d
shimon <69175188+lil-shimon@users.noreply.github.com>
2022-06-12 22:33:51 +0900

    feat: 🎸 use cases

---
+++ app/src/main/java/com/example/crypt_currency_app/domain/use_case/get_coin/GetCoinUseCase.kt
@@ -0,0 +1,27 @@
+package com.example.crypt_currency_app.domain.use_case.get_coin
+
+import com.example.crypt_currency_app.common.Resource
+import com.example.crypt_currency_app.data.remote.dto.toCoinDetail
+import com.example.crypt_currency_app.domain.model.CoinDetail
+import com.example.crypt_currency_app.domain.repository.CoinRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class GetCoinUseCase @Inject constructor(
+    private val repository: CoinRepository
+) {
+    operator fun invoke(coinId: String): Flow<Resource<CoinDetail>> = flow {
+        try {
+            emit(Resource.Loading())
+            val coins = repository.getCoinById(coinId).toCoinDetail()
+            emit(Resource.Success(coins)) // forwards to vm
+        } catch (e: HttpException) {
+            emit(Resource.Error(e.localizedMessage ?: "An expected error occurred"))
+        } catch (e: IOException) {
+            emit(Resource.Error("Could not reach server. Check your internet connection"))
+        }
+    }
+}
---
+++ app/src/main/java/com/example/crypt_currency_app/domain/use_case/get_coins/GetCoinsUseCase.kt
@@ -0,0 +1,27 @@
+package com.example.crypt_currency_app.domain.use_case.get_coins
+
+import com.example.crypt_currency_app.common.Resource
+import com.example.crypt_currency_app.data.remote.dto.toCoin
+import com.example.crypt_currency_app.domain.model.Coin
+import com.example.crypt_currency_app.domain.repository.CoinRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class GetCoinsUseCase @Inject constructor(
+    private val repository: CoinRepository
+) {
+    operator fun invoke(): Flow<Resource<List<Coin>>> = flow {
+        try {
+            emit(Resource.Loading())
+            val coins = repository.getCoins().map { it.toCoin() }
+            emit(Resource.Success(coins)) // forwards to vm
+        } catch (e: HttpException) {
+            emit(Resource.Error(e.localizedMessage ?: "An expected error occurred"))
+        } catch (e: IOException) {
+            emit(Resource.Error("Could not reach server. Check your internet connection"))
+        }
+    }
+}
