bb50290a3b58120aa1c5b1a17fff1345adca0779
shimon <69175188+lil-shimon@users.noreply.github.com>
2022-06-13 08:20:33 +0900

    feat: 🎸 added two components

---
+++ app/src/main/java/com/example/crypt_currency_app/presentation/coin_detail/components/TeamListItem.kt
@@ -0,0 +1,40 @@
+package com.example.crypt_currency_app.presentation.coin_detail.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.unit.dp
+import com.example.crypt_currency_app.data.remote.dto.TeamMember
+
+@Composable
+fun TeamListItem(
+    teamMember: TeamMember,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            modifier = modifier,
+            text = teamMember.name,
+            style = MaterialTheme.typography.h4
+        )
+
+        Spacer(
+            modifier = Modifier.height(4.dp),
+        )
+
+        Text(
+            text = teamMember.position,
+            style =  MaterialTheme.typography.body2,
+            fontStyle = FontStyle.Italic
+        )
+    }
+}
